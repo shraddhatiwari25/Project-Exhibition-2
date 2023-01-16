832152f14ac7dd36b224abad95fb5df41b030757
Shraddha <tiwarishraddha499@gmail.com>
2023-01-16 17:56:14 +0530

    Worker Form

--- app/src/main/java/com/project/aiohelp/DBManipulation.kt
+++ app/src/main/java/com/project/aiohelp/DBManipulation.kt
@@ -3,6 +3,7 @@ package com.project.aiohelp
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import com.project.aiohelp.data.UserModel
+import com.project.aiohelp.data.workerModel
 
 class DBManipulation {
     private val database = Firebase.database("https://aio-help-default-rtdb.asia-southeast1.firebasedatabase.app")
@@ -11,5 +12,11 @@ class DBManipulation {
     public fun addCustomer(name: String, email: String, pass: String){
         val user = UserModel(name, email, pass)
         dbRef.child(email).setValue(user)
+    }
+
+    public fun addWorker(name: String,age: Int,email: String,pass: String,job: String){
+        val worker = workerModel(name,age,email,pass,job)
+        val dbRef = database.getReference("worker")
+        dbRef.child(email).setValue(worker)
     }
 }
---
+++ app/src/main/java/com/project/aiohelp/data/WorkerForm.kt
@@ -0,0 +1,10 @@
+package com.project.aiohelp.data
+
+data class workerModel (
+    var name: String? = null,
+    var age:Int?=null,
+    var email: String? = null,
+    var password: String? = null,
+    var job:String?=null
+
+)
